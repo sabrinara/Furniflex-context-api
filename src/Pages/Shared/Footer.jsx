df68155764a530c038dd0e1ff8c2d9f1b8f94fe4
TechBuilderTeam <saashid18@gmail.com>
2024-09-08 05:12:49 +0600

    footer done

--- src/Pages/Shared/Footer.jsx
+++ src/Pages/Shared/Footer.jsx
@@ -1,10 +1,92 @@
-
-
+import { FaFacebook, FaInstagram, FaTwitter, } from "react-icons/fa";
+import Logo from "../../../public/logo.png";
+import { FaLinkedin } from "react-icons/fa6";
 const Footer = () => {
     return (
-        <div>
-<h1>Footer</h1>
-        </div>
+        <footer className="w-full p-12 bg-black">
+
+            <div className="flex flex-col lg:flex-row items-center justify-center gap-y-10   text-center md:justify-between">
+                <div className="flex  items-center  md:mr-10 lg:ml-20 ">
+                    <img className="w-18 h-20" src={Logo} alt="" />
+
+                </div>
+
+                <ul className="flex flex-col lg:flex-row flex-wrap items-start gap-y-2 gap-x-14 lg:mr-20">
+                    <li>
+                        <a
+                            href="/about"
+                            className="block font-sans text-xl font-bold leading-relaxed text-blue-gray-900 antialiased transition-colors hover:text-cyan-800 focus:text-cyan-800"
+                        >
+                            About Us
+                        </a>
+                        <div className="flex flex-col text-start mt-4 text-sm">
+                            <h1>Master Plan</h1>
+                            <h1>Jobs</h1>
+                            <h1>Invest</h1>
+                            <h1>Pressroom</h1>
+                            <h1>Invest</h1>
+                            <h1>Blog</h1>
+                            <h1>Contact</h1>
+                        </div>
+                    </li>
+
+                    <li>
+                        <a
+                            href="#"
+                            className="block font-sans text-xl font-bold leading-relaxed text-blue-gray-900 antialiased transition-colors hover:text-cyan-800 focus:text-cyan-800"
+                        >
+                            Explore EEVE
+                        </a>
+                        <div className="flex flex-col items-start mt-4 text-sm">
+                            <h1>Unlock my Robot Power</h1>
+                            <h1>Starlight</h1>
+                            <h1>Robot Platform</h1>
+                            <h1>EEVE Roadmap</h1>
+                        </div>
+                    </li>
+                    <li>
+                        <a
+                            href="#"
+                            className="block font-sans text-xl font-bold leading-relaxed text-blue-gray-900 antialiased transition-colors hover:text-cyan-800 focus:text-cyan-800"
+                        >
+                            Community & Support
+                        </a>
+                        <div className="flex flex-col items-start mt-4 text-sm">
+                            <h1>Willow X Community</h1>
+                            <h1>Developer & Maker Access</h1>
+                            <h1>Special Cases</h1>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <hr className="my-8 border-sky-400 lg:mx-24 lg:hidden" />
+
+
+            <hr className="my-8 border-w " />
+            <div className="flex flex-col items-center justify-center gap-4 lg:flex-row lg:justify-between">
+                <div className="grid grid-flow-col gap-6 text-white m-6 justify-center " target="blank">
+                    <a rel="noopener noreferrer" href="https://www.facebook.com/" target="_blank" ><FaFacebook className="md:w-5 md:h-5 "></FaFacebook></a>
+                    <a rel="noopener noreferrer" href="https://www.instagram.com/" target="_blank"><FaInstagram className="md:w-5 md:h-5" ></FaInstagram></a>
+                    <a rel="noopener noreferrer" href="https://twitter.com/" target="_blank"><FaTwitter className="md:w-5 md:h-5" ></FaTwitter></a>
+                    <a rel="noopener noreferrer" href="https://www.linkedin.com/" target="_blank"><FaLinkedin className="md:w-5 md:h-5" ></FaLinkedin></a>
+                </div>
+                <div className="flex  items-center gap-4">
+                    <h1>March22 Recap</h1>
+                    <h1>Privacy Policy</h1>
+                    <h1>General Terms</h1>
+                    <h1>Contact</h1>
+                </div>
+                <div>
+                    <h1>🇺🇸
+                    United States (English)</h1>
+                </div>
+            </div>
+            <p className="block text-center font-sans text-base font-normal leading-relaxed text-blue-gray-900 antialiased">EEVE
+                © 2024, All rights reserved
+            </p>
+
+        </footer >
+
     );
 };
 
